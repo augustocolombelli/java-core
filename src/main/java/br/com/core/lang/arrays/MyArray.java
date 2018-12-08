1bd14469dd95bbb50d617e5bdb57837b15de6c58
Augusto Colombelli Alessio <augustocolombelli@gmail.com>
2018-12-07 23:35:26 -0200

    add element in a specific position

--- src/main/java/br/com/core/lang/arrays/MyArray.java
+++ src/main/java/br/com/core/lang/arrays/MyArray.java
@@ -1,8 +1,9 @@
 package br.com.core.lang.arrays;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
-public class MyArray<E> implements Iterable<E>{
+public class MyArray<E> implements Iterable<E> {
 
 	private Object[] elements;
 	private int actualIndex;
@@ -11,9 +12,17 @@ public class MyArray<E> implements Iterable<E>{
 		this.elements = new Object[positions];
 		this.actualIndex = 0;
 	}
-	
+
 	public void add(E element) {
 		elements[actualIndex] = element;
+		actualIndex++;
+	}
+
+	public void add(E element, int position) {
+		for(int i = actualIndex - 1; i >= position; i--) {
+			elements[i + 1] = elements[i];
+		}
+		elements[position] = element;
 		actualIndex++;
 	}
 	
@@ -21,48 +30,60 @@ public class MyArray<E> implements Iterable<E>{
 	public E get(int index) {
 		return (E) elements[index];
 	}
-	
+
 	public void remove(E elementToRemove) {
-		for(int i = 0; i < actualIndex; i++) {
-			//Encontrou o elemento
-			if(elements[i].equals(elementToRemove) ) {
+		for (int i = 0; i < actualIndex; i++) {
+			// Encontrou o elemento
+			if (elements[i].equals(elementToRemove)) {
 				// Salva a posicao dele
 				int indexRemoved = i;
 				// Percorre do elemento removido até o final da lista
-				for(int j = indexRemoved; j< actualIndex; j++) {
+				for (int j = indexRemoved; j < actualIndex; j++) {
 					// adiciona ao elemento o elemento posterior
-					elements[j] = elements[j+1];
+					elements[j] = elements[j + 1];
 				}
 				actualIndex--;
 			}
 		}
 	}
-	
+
+	public String toString() {
+		return Arrays.toString(elements);
+	}
+
 	@Override
 	public Iterator<E> iterator() {
 		Iterator<E> it = new MyArrayIterator<E>(this.elements, actualIndex);
 		return it;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		MyArray<String> strings = new MyArray<>(5);
 		strings.add("A");
 		strings.add("B");
 		strings.add("C");
-		
+
 		System.out.println("Printing with all values...");
-		for(String s : strings) {
+		for (String s : strings) {
 			System.out.println(s);
 		}
-		
+
 		strings.remove("B");
 		strings.remove("A");
-		
+
 		System.out.println("Printing after remove values..");
-		for(String s : strings) {
+		for (String s : strings) {
 			System.out.println(s);
 		}
+
+		System.out.println("Printing after add in a specific position...");
+		strings.add("T");
+		strings.add("Z");
+		System.out.println(strings.toString());
+		//strings.add("K", 1);
+		strings.add("K", 1);
+		strings.add("LL", 3);
+		System.out.println(strings.toString());
 	}
 
 }
