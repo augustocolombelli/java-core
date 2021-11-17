6f6c5db6628d20d393e65997d4c1a2304274447e
Augusto <augustocolombelli@gmail.com>
2021-11-17 17:36:04 -0300

    renaming package

--- java-core-instant/src/main/java/com/playground/InstantPlayground.java
+++ java-core-instant/src/main/java/com/playground/InstantPlayground.java
@@ -1,4 +1,4 @@
-package com.core;
+package com.playground;
 
 import java.time.Duration;
 import java.time.Instant;
--- java-core-instant/src/test/java/com/playground/IntervalPlaygroundTest.java
+++ java-core-instant/src/test/java/com/playground/IntervalPlaygroundTest.java
@@ -1,4 +1,4 @@
-package com.core;
+package com.playground;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
